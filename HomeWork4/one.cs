73c1b6a31009bc7b16d17d7602c4bd6cce4a3cc4
fsepic <fsepicfs@gmail.com>
2020-03-09 16:38:26 +0800

    第四次作业上传

---
+++ HomeWork4/one.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Foreach(Action<T> action)
+        {
+            Node<T> n;
+            n = this.head;
+            while (n!=null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            /*for (Node<int> node = intlist.Head;
+                  node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }*/
+            intlist.Foreach(s => Console.WriteLine(s));
+            int max=int.MinValue;
+            intlist.Foreach(n => { max = max < n ? n : max; } );
+            Console.WriteLine($"max:{max}");
+            int min = int.MaxValue;
+            intlist.Foreach(n => { min = min > n ? n : min; });
+            Console.WriteLine($"min:{min}");
+            int total = 0;
+            intlist.Foreach(n =>  total += n );
+            Console.WriteLine(total);
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            /*for (Node<string> node = strList.Head;
+                    node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }*/
+            strList.Foreach(s => Console.WriteLine(s));
+
+        }
+
+    }
+}
---
+++ HomeWork4/two.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+
+namespace _2020_3_9_two
+{
+    
+    public delegate void AlarmTick(Object a,Alarm t);
+    public delegate void Alarmgo(Object a,Alarm t);
+    
+    public class Alarm
+    {
+        public int S { set; get; }
+        public int M { set; get; }
+        public int H { set; get; }
+
+        public event AlarmTick Tick;
+        public event Alarmgo alar;
+        public void Alarmtick()
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                S++;
+                if (S == 61)
+                {
+                    M++;
+                    S = 0;
+                }
+                if (M == 61)
+                {
+                    H++;
+                    M = 0;
+                }
+                if (H == 25)
+                {
+                    H = 1;
+                }
+                Tick(null,this);
+                alar(null,this);
+            }
+        }
+        public Alarm(string s,string m,string h)
+        {
+            S = int.Parse(s);
+            M = int.Parse(m);
+            H = int.Parse(h);
+        }
+
+        
+    }
+    class Program
+    {
+        void tick_print(object a,Alarm t)
+        {
+            Action<int> action = s => Console.WriteLine(s);
+            action(t.S);
+            action(t.M);
+            action(t.H);
+        }
+        void alarm_setting(object a, Alarm t)
+        {
+            if (t.S == 14 && t.M == 19 && t.H == 11) Console.WriteLine("时间到了");
+        }
+        static void Main(string[] args)
+        {
+            Alarm a = new Alarm(DateTime.Now.Second.ToString(), DateTime.Now.Minute.ToString(), DateTime.Now.Hour.ToString());
+            Program p = new Program();
+            a.Tick += p.tick_print;
+            a.alar += p.alarm_setting;
+            a.Alarmtick();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
